2f47ead4e69194cd641e4dbdf6aaa629aeb7dde4
feysal <iamfeysal@gmail.com>
2022-06-09 02:49:48 +0300

    include share links

--- src/components/BlogDetail.vue
+++ src/components/BlogDetail.vue
@@ -49,6 +49,12 @@
           </ul>
           <div class="share">
             <div class="sharepost">
+              <li><a class="twitter" href="http://twitter.com/share?url={{  request.build_absolute_uri }}" rel="nofollow" target="_blank" title="Twitter Tweet">
+                <i class="fa fa-twitter"></i>&nbsp; Tweet</a></li>
+              <li><a class="facebook" href="https://www.linkedin.com/sharing/share-offsite/?mini=true&url={{  request.build_absolute_uri }}" rel="nofollow" target="_blank" title="Facebook Share">
+                <i class="fa fa-linkedin"></i>&nbsp; Share</a></li>
+              <li><a class="facebook" href="http://www.facebook.com/sharer.php?u={{  request.build_absolute_uri }}" rel="nofollow" target="_blank" title="Facebook Share">
+                <i class="fa fa-facebook"></i>&nbsp; Share</a></li>
             </div>
           </div>
         </article>
@@ -126,6 +132,6 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
