b47d3abc6f5db3994e856973f68b2d7ed5c15a84
feysal <iamfeysal@gmail.com>
2022-06-11 23:52:26 +0300

    set up correct states

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-// import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 
-// Vue.use(Vuex)
-
-export default new Vuex.Store({
+// Create a new store instance.
+const store = createStore({
     state: {
-    },
-    mutations: {
-    },
-    actions: {
-    },
-    modules: {
+        posts: []
     }
 })
+const mutations = {
+
+};
+
+const actions = {}
+export default{
+    namespaced: true,
+    store,
+    mutations,
+    actions
+};
